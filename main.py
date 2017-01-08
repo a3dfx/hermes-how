9003898588100406476abf9cec1f4a5bc57b5adf
Liron Shapira <liron@quixey.com>
2017-01-08 14:08:25 -0800

    Migrated from App Engine to Firebase static hosting

--- main.py
+++
@@ -1,9 +0,0 @@
-import webapp2
-
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.out.write(open('index.html').read())
-
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
