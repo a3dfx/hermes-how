43b4f8b19706b6cf36bf50bdd72bfb6086906ed3
Liron Shapira <liron@quixey.com>
2017-01-08 22:18:27 -0800

    Google conversion tracking

--- public/static/js/site.js
+++ public/static/js/site.js
@@ -51,7 +51,7 @@ var toggleModal = function(toShow) {
 }
 
 var directToHermes = function() {
-	window.location.href = "https://hermes.social/trial";
+	goog_report_conversion('https://hermes.social/trial');
 }
 
 // Header Animations
@@ -85,3 +85,27 @@ $(window).bind("load", function() {
 	});
 
 });
+
+
+
+// Adwords conversion tracking stuff
+goog_snippet_vars = function() {
+  var w = window;
+  w.google_conversion_id = 863472388;
+  w.google_conversion_label = "ve1-CKyhmW0QhJbemwM";
+  w.google_remarketing_only = false;
+}
+goog_report_conversion = function(url) {
+  goog_snippet_vars();
+  window.google_conversion_format = "3";
+  var opt = new Object();
+  opt.onload_callback = function() {
+	  if (typeof(url) != 'undefined') {
+	    window.location = url;
+	  }
+	}
+	var conv_handler = window['google_trackConversion'];
+	if (typeof(conv_handler) == 'function') {
+	  conv_handler(opt);
+	}
+}
